8028e5817a1529e7b9e6db33898be9445fcec199
G-r-e-g-R <grodrper@emeal.nttdata.com>
2023-04-13 20:46:22 -0500

    Jarvis - adicionando el manifest

--- src/main/java/com/bcp/pe/exchange/infraestructure/security/JWTAuthorizationFilter.java
+++ src/main/java/com/bcp/pe/exchange/infraestructure/security/JWTAuthorizationFilter.java
@@ -44,6 +44,7 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
         } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException e) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             ((HttpServletResponse) response).sendError(HttpServletResponse.SC_FORBIDDEN, e.getMessage());
+            e.printStackTrace();
             return;
         }
     }
